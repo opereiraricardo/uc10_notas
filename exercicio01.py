d300e97a23f18b68da6389f4d865c98c02800a84
Ricardo <opereira.ricardo@gmail.com>
2023-11-01 15:02:57 -0300

    Codigo de Notas

---
+++ exercicio01.py
@@ -0,0 +1,14 @@
+print("Exercicio de GitHub")
+
+nome = input("Digite o seu Nome: ")
+
+print(f"O nome digitado foi: {nome}")
+
+nota1 = float(input("Digite uma nota: "))
+nota2 = float(input("Digite uma nota: "))
+nota3 = float(input("Digite uma nota: "))
+nota4 = float(input("Digite uma nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print(f"O aluno: {nome} \nObteve a média: {media}")
